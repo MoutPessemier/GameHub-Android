cdc4b1045a5fbb19ac85782b83db11ed2add0377
Mout Pessemier <moutpessemier@hotmail.com>
2019-10-11 16:02:47 +0200

    V1.0.2 - login layout - added login - added GameHubApplication

---
+++ app/src/main/java/digitized/gamehub/GameHubApplication.kt
@@ -0,0 +1,16 @@
+package digitized.gamehub
+
+import android.app.Application
+import timber.log.Timber
+
+class GameHubApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
+
+    }
+}
