e3e725cb099b6f8154ba0b0c7c00515405afbd2e
Mout Pessemier <moutpessemier@hotmail.com>
2019-11-06 18:34:04 +0100

    V1.1.5 - added google maps activity + api [FEAT] - added maps activity - linked up google maps api
    [CHANGES]
    - login and register layout

---
+++ app/src/main/java/digitized/gamehub/map/MapsActivity.kt
@@ -0,0 +1,65 @@
+package digitized.gamehub.map
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.location.*;
+import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import digitized.gamehub.R
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit  var locationManager: LocationManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+//        locationManager.requestLocationUpdates(0, 100f, Criteria(), {
+//            location: Location -> Timber.i(location.toString())
+//        })
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val ghent = LatLng(51.0543, 3.7174)
+        mMap.addMarker(MarkerOptions().position(ghent).title("Marker in Ghent"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ghent))
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+       //deprecated
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+        Toast.makeText(applicationContext, "Enable your location please.", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
