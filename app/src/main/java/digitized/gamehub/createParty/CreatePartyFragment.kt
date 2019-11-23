5bd02aa1e9824301ce88809de380f41f3c593358
Mout Pessemier <moutpessemier@hotmail.com>
2019-11-23 19:13:01 +0100

    V1.1.22 - create Party [WIP FEAT] - autocomplete fragment - the create request
    [FEAT]
    - layout changes

--- app/src/main/java/digitized/gamehub/createParty/CreatePartyFragment.kt
+++ app/src/main/java/digitized/gamehub/createParty/CreatePartyFragment.kt
@@ -1,28 +1,44 @@
 package digitized.gamehub.createParty
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import digitized.gamehub.R
 import digitized.gamehub.databinding.CreateGamePartyBinding
+import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class CreatePartyFragment : Fragment() {
 
     private lateinit var viewModel: CreatePartyViewModel
+    private lateinit var binding: CreateGamePartyBinding
+    private lateinit var spinnerAdapter: ArrayAdapter<String>
+
+    companion object {
+        private const val AUTOCOMPLETE_REQUEST_CODE = 1
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: CreateGamePartyBinding =
+        binding =
             DataBindingUtil.inflate(
                 inflater,
                 R.layout.create_game_party, container, false
@@ -30,23 +46,89 @@ class CreatePartyFragment : Fragment() {
 
         // ViewModel
         val application = requireNotNull(activity).application
-        viewModel = ViewModelProviders.of(this, CreatePartyViewModel.Factory(application)).get(CreatePartyViewModel::class.java)
+        viewModel = ViewModelProviders.of(this, CreatePartyViewModel.Factory(application))
+            .get(CreatePartyViewModel::class.java)
         binding.viewModel = viewModel
 
-        // val spinnerAdapter = ArrayAdapter<String>(context!!) do I need this?
-        // binding.sprPartyGame.adapter = spinnerAdapter
+        spinnerAdapter = ArrayAdapter(
+            this.requireContext(),
+            android.R.layout.simple_spinner_dropdown_item
+        )
 
-        binding.btnCreateGamParty.setOnClickListener { view: View ->
-            //viewModel.createParty(binding.txtPartyName.text.toString(), SimpleDateFormat(binding.txtPartyDate.text.toString()))
-            view.findNavController()
-                .navigate(CreatePartyFragmentDirections.actionNavCreatePartyViewToGameCardsFragment())
+        if (!Places.isInitialized()) {
+            Places.initialize(
+                requireActivity().applicationContext,
+                getString(R.string.google_maps_key)
+            )
         }
 
-        binding.btnChooseLocation.setOnClickListener{
-            val activity = Intent(context, AutocompleteActivity::class.java)
-            startActivity(activity)
+        val placesClient = Places.createClient(requireActivity().applicationContext)
+
+
+        binding.sprPartyGame.adapter = spinnerAdapter
+        binding.sprPartyGame.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val game = viewModel.games.value!![position]
+                viewModel.game = game
+            }
+
         }
 
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val autocompleteFragment =
+            childFragmentManager
+                .findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
+
+        autocompleteFragment.setPlaceFields(
+            listOf(
+                Place.Field.ID,
+                Place.Field.NAME,
+                Place.Field.LAT_LNG,
+                Place.Field.ADDRESS
+            )
+        )
+
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                Timber.d("Place: %s, %s, %s, %s", place.name, place.id, place.latLng, place.address)
+                viewModel.place = place
+            }
+
+            override fun onError(status: Status) {
+                Timber.d("An error occurred: $status")
+            }
+        })
+
+        viewModel.games.observe(viewLifecycleOwner, Observer {
+            spinnerAdapter.clear()
+            spinnerAdapter.addAll(it.map { game -> game.name })
+        })
+
+        binding.btnCreateGamParty.setOnClickListener { view: View ->
+            val succes = viewModel.createParty(
+                binding.txtPartyName.text.toString(),
+                SimpleDateFormat().parse(binding.txtPartyDate.text.toString())!!,
+                binding.txtMaxSize.text.toString().toInt()
+            )
+            if (succes) {
+                binding.txtPartyName.text.clear()
+                binding.txtMaxSize.text.clear()
+                binding.txtPartyDate.text.clear()
+            }
+            view.findNavController()
+                .navigate(CreatePartyFragmentDirections.actionCreatePartyFragmentToCardStackFragment())
+        }
     }
 }
--- app/src/main/java/digitized/gamehub/createParty/CreatePartyViewModel.kt
+++ app/src/main/java/digitized/gamehub/createParty/CreatePartyViewModel.kt
@@ -4,22 +4,73 @@ import android.app.Application
 import androidx.lifecycle.*
 import com.google.android.libraries.places.api.model.Place
 import digitized.gamehub.database.GameHubDatabase.Companion.getInstance
+import digitized.gamehub.domain.ApiStatus
+import digitized.gamehub.domain.Game
 import digitized.gamehub.domain.GameParty
 import digitized.gamehub.repositories.GameRepository
+import java.util.*
+import digitized.gamehub.domain.Location
+import digitized.gamehub.network.GameHubAPI
+import digitized.gamehub.repositories.PartyRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class CreatePartyViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val _place = MutableLiveData<Place>()
-    val place: LiveData<Place>
-        get() = _place
+    private val viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    private val _status = MutableLiveData<ApiStatus>()
+    val status: LiveData<ApiStatus>
+        get() = _status
 
     private val database = getInstance(application)
     private val gameRepository = GameRepository(database)
+    private val partyRepository = PartyRepository(database)
 
     val games = gameRepository.games
 
-    fun setPlace(place: Place) {
-        _place.value = place
+    var game: Game? = null
+    var place: Place? = null
+
+    fun createParty(partyName: String, whenDate: Date, maxSize: Int): Boolean {
+        val createdParty = GameParty(
+            null,
+            partyName,
+            whenDate,
+            maxSize,
+            arrayOf<String>(""),
+            game!!.id!!,
+            Location(
+                "Point",
+                doubleArrayOf(place!!.latLng!!.latitude, place!!.latLng!!.longitude)
+            )
+        )
+        try {
+            coroutineScope.launch {
+                _status.value = ApiStatus.LOADING
+                val addedParty = GameHubAPI.service.createParty(createdParty).await()
+                _status.value = ApiStatus.DONE
+                val domainParty = GameParty(
+                    addedParty.id,
+                    addedParty.name,
+                    addedParty.date,
+                    addedParty.maxSize,
+                    addedParty.participants,
+                    addedParty.gameId,
+                    addedParty.location
+                )
+                partyRepository.createParty(domainParty)
+
+            }
+            return true
+        } catch (e: Exception) {
+            _status.value = ApiStatus.ERROR
+        }
+        return false
     }
 
     class Factory(val app: Application) : ViewModelProvider.Factory {
